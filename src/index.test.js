d71614dc9d4aff0265590d37db14b53f2c63cfaf
Frazer Smith <frazer.dev@icloud.com>
2025-11-18 15:28:07 +0000

    test(index): check directory path exists (#12)

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,8 @@
+/* eslint-disable security/detect-non-literal-fs-filename -- Test files are not user-provided */
+
 "use strict";
 
-const { join } = require("node:path");
+const { stat } = require("node:fs/promises");
 // eslint-disable-next-line n/no-unsupported-features/node-builtins -- Tests, not in distributed code
 const { describe, it } = require("node:test");
 const binaryPath = require("./index");
@@ -8,9 +10,9 @@ const binaryPath = require("./index");
 /** @typedef {import('node:test').TestContext} TestContext */
 
 describe("node-unrtf-win32 export", () => {
-	it("Returns the correct UnRTF binary directory", (/** @type {TestContext} */ t) => {
+	it("Returns the correct Unrtf binary directory", async (/** @type {TestContext} */ t) => {
 		t.plan(1);
-		const expectedSuffix = join("lib", "unrtf-0.19.3", "bin");
-		t.assert.strictEqual(binaryPath.endsWith(expectedSuffix), true);
+		const stats = await stat(binaryPath);
+		t.assert.strictEqual(stats.isDirectory(), true);
 	});
 });
